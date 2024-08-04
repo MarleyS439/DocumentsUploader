c473115e783dcb4bf8d50f4dc222fe21cde75496
MarleyS439 <marleysantos439@gmail.com>
2024-08-04 16:36:27 -0300

    Base

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-br" dir="ltr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, intial-scale=1">
+		<title>Envio de Documentos</title> 
+		<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+	</head>
+	
+	<body>
+		<header></header>
+		<main>
+			<a href="view/form.php">Acessar</a>
+		</main>
+		<footer></footer>
+	</body>
+</html>
---
+++ view/form.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="pt-br" dir="ltr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, intial-scale=1">
+		<title>Envio de Documentos</title> 
+		<link rel="stylesheet" type="text/css" href="assets/css/form.css">
+	</head>
+	
+	<body>
+		<div class="form">
+			<h1>Formulário de Envio de Documentos</h1>
+		</div>
+	</body>
+</html>
